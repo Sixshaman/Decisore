8bca3da63fccb9bcddf668ea777612fce2ad2a56
Sixshaman <Shaman1234@gmail.com>
2020-03-24 17:31:09 +0300

    Task add ui

--- app/src/main/java/com/sixshaman/advancedunforgetter/ui/TaskListActivity.java
+++ app/src/main/java/com/sixshaman/advancedunforgetter/ui/TaskListActivity.java
@@ -1,13 +1,18 @@
 package com.sixshaman.advancedunforgetter.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 import com.sixshaman.advancedunforgetter.R;
 import com.sixshaman.advancedunforgetter.archive.TaskArchive;
@@ -15,6 +20,7 @@ import com.sixshaman.advancedunforgetter.list.TaskList;
 import com.sixshaman.advancedunforgetter.scheduler.TaskScheduler;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class TaskListActivity extends AppCompatActivity
 {
@@ -45,7 +51,7 @@ public class TaskListActivity extends AppCompatActivity
         FloatingActionButton buttonNewTask = findViewById(R.id.addNewTask);
         buttonNewTask.setOnClickListener(view ->
         {
-            mTaskScheduler.addImmediateTask("LOL", "", new ArrayList<>());
+            openAddTaskDialog();
         });
 
         RecyclerView recyclerView = findViewById(R.id.taskListView);
@@ -56,8 +62,63 @@ public class TaskListActivity extends AppCompatActivity
         mTaskList.setUiAdapter(adapter);
     }
 
-    public void updateTasksUi()
+    private void openAddTaskDialog()
     {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
 
+        final LayoutInflater inflater = getLayoutInflater();
+        alertBuilder.setView(inflater.inflate(R.layout.layout_dialog_new_task, null));
+
+        alertBuilder.setTitle(R.string.newTaskDialogName);
+
+        alertBuilder.setPositiveButton(R.string.createTask, (dialogInterface, i) ->
+        {
+            final EditText editTextName         = ((AlertDialog)dialogInterface).findViewById(R.id.editTaskName);
+            final EditText editTextNDescription = ((AlertDialog)dialogInterface).findViewById(R.id.editTaskName);
+
+            final Spinner taskTypeSpinner = ((AlertDialog)dialogInterface).findViewById(R.id.spinnerTaskType);
+
+            assert editTextName         != null;
+            assert editTextNDescription != null;
+            assert taskTypeSpinner      != null;
+
+            String nameText = editTextName.getEditableText().toString();
+            if(nameText.isEmpty())
+            {
+                Toast toast = Toast.makeText(TaskListActivity.this, R.string.invalidTaskName, Toast.LENGTH_SHORT);
+                toast.show();
+            }
+            else
+            {
+                String descriptionText = editTextNDescription.getEditableText().toString();
+
+                int taskTypeIndex = taskTypeSpinner.getSelectedItemPosition();
+                switch(taskTypeIndex)
+                {
+                    //Immediate task
+                    case 0:
+                        mTaskScheduler.addImmediateTask(nameText, descriptionText, new ArrayList<>());
+                        break;
+
+                    //Deferred task
+                    case 1:
+                        break;
+
+                    //Regular task
+                    case 2:
+                        break;
+
+                    //Irregular task
+                    case 3:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+
+        AlertDialog dialog = alertBuilder.create();
+        dialog.show();
     }
 }
