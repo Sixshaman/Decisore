f5b256d8ed981f7f4ee8945bec958b36da2bfd45
Sixshaman <shaman1234@gmail.com>
2021-02-28 23:57:10 +0300

    App name fix

--- app/src/main/java/com/sixshaman/advancedunforgetter/list/ListActivity.java
+++ app/src/main/java/com/sixshaman/advancedunforgetter/list/ListActivity.java
@@ -31,6 +31,9 @@ public class ListActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbarTaskList);
         setSupportActionBar(toolbar);
 
+        //The only way to get the name for the application right
+        setTitle(R.string.title_activity_task_list);
+
         FloatingActionButton buttonNewTask = findViewById(R.id.addNewTask);
         buttonNewTask.setOnClickListener(view -> openAddTaskDialog());
     }
