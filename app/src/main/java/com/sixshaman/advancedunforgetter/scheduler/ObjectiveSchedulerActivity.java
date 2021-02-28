4a58a1ea88392aaeb4dc67c248ddd6cbdffe110b
Sixshaman <shaman1234@gmail.com>
2021-02-28 22:53:29 +0300

    New add dialog design

---
+++ app/src/main/java/com/sixshaman/advancedunforgetter/scheduler/ObjectiveSchedulerActivity.java
@@ -0,0 +1,85 @@
+package com.sixshaman.advancedunforgetter.scheduler;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.widget.*;
+import androidx.appcompat.app.AlertDialog;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.View;
+import com.sixshaman.advancedunforgetter.R;
+import com.sixshaman.advancedunforgetter.list.ObjectiveListCache;
+import com.sixshaman.advancedunforgetter.utils.LockedReadFile;
+import com.sixshaman.advancedunforgetter.utils.NewObjectiveDialogFragment;
+import com.sixshaman.advancedunforgetter.utils.TransactionDispatcher;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class ObjectiveSchedulerActivity extends AppCompatActivity
+{
+    //Scheduler cache model
+    private ObjectiveSchedulerCache mSchedulerCache;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_scheduler);
+        Toolbar toolbar = findViewById(R.id.toolbar_scheduler);
+        setSupportActionBar(toolbar);
+
+        setTitle(R.string.title_activity_task_scheduler);
+
+        FloatingActionButton fab = findViewById(R.id.fab_new_scheduled_task);
+        fab.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //COPIED FROM TASKLISTACTIVITY
+                openAddTaskDialog();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        mSchedulerCache = new ObjectiveSchedulerCache();
+
+        String configFolder = Objects.requireNonNull(getExternalFilesDir("/app")).getAbsolutePath();
+
+        try
+        {
+            LockedReadFile listFile = new LockedReadFile(configFolder + "/" + ObjectiveListCache.LIST_FILENAME);
+            mSchedulerCache.invalidate(listFile);
+            listFile.close();
+
+            TransactionDispatcher transactionDispatcher = new TransactionDispatcher();
+            transactionDispatcher.setSchedulerCache(mSchedulerCache);
+
+            transactionDispatcher.updateObjectiveListTransaction(configFolder, LocalDateTime.now());
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private void openAddTaskDialog()
+    {
+        NewObjectiveDialogFragment newObjectiveDialogFragment = new NewObjectiveDialogFragment();
+        newObjectiveDialogFragment.show(getSupportFragmentManager(), "Test");
+    }
+}
---
+++ app/src/main/java/com/sixshaman/advancedunforgetter/utils/NewObjectiveDialogFragment.java
@@ -0,0 +1,212 @@
+package com.sixshaman.advancedunforgetter.utils;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.DialogFragment;
+import com.sixshaman.advancedunforgetter.R;
+import com.sixshaman.advancedunforgetter.list.ListActivity;
+import com.sixshaman.advancedunforgetter.list.ObjectiveListCache;
+import com.sixshaman.advancedunforgetter.scheduler.ObjectiveSchedulerCache;
+import com.sixshaman.advancedunforgetter.scheduler.SingleObjectiveSource;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class NewObjectiveDialogFragment extends DialogFragment
+{
+    private ObjectiveSchedulerCache mSchedulerCache;
+    private ObjectiveListCache      mListCache;
+
+    public NewObjectiveDialogFragment()
+    {
+    }
+
+    public void setSchedulerCache(ObjectiveSchedulerCache schedulerCache)
+    {
+        mSchedulerCache = schedulerCache;
+    }
+
+    public void setListCache(ObjectiveListCache listCache)
+    {
+        mListCache = listCache;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        Activity activity = getActivity();
+
+        final ValueHolder<AlertDialog> resultDialog = new ValueHolder<>(null);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        LayoutInflater inflater = activity.getLayoutInflater();
+
+        builder.setView(inflater.inflate(R.layout.layout_dialog_new_task, null));
+        builder.setTitle(R.string.newTaskDialogName);
+
+        builder.setPositiveButton(R.string.createTask, new DialogInterface.OnClickListener()
+        {
+            @Override
+            public void onClick(DialogInterface dialog, int id)
+            {
+                final EditText editTextName         = resultDialog.getValue().findViewById(R.id.editTaskName);
+                final EditText editTextNDescription = resultDialog.getValue().findViewById(R.id.editTaskDescription);
+
+                final Spinner  scheduleSpinner    = resultDialog.getValue().findViewById(R.id.spinnerObjectiveSchedule);
+                final Spinner  repeatSpinner      = resultDialog.getValue().findViewById(R.id.spinnerObjectiveRepeats);
+                final Spinner  intervalSpinner    = resultDialog.getValue().findViewById(R.id.spinnerObjectiveInterval);
+                final CheckBox occasionalCheckbox = resultDialog.getValue().findViewById(R.id.checkboxOccasional);
+
+                String nameText = editTextName.getEditableText().toString();
+                if(nameText.isEmpty())
+                {
+                    Toast toast = Toast.makeText(activity, R.string.invalidTaskName, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else
+                {
+                    String descriptionText = editTextNDescription.getEditableText().toString();
+
+                    LocalDateTime objectiveCreateDate                      = LocalDateTime.now();
+                    final ValueHolder<LocalDateTime> objectiveScheduleDate = new ValueHolder<>(objectiveCreateDate);
+
+                    Duration objectiveRepeatDuration = Duration.ZERO;
+                    float objectiveRepeatProbability = 0.0f;
+
+                    int objectiveScheduleIndex = scheduleSpinner.getSelectedItemPosition();
+                    switch(objectiveScheduleIndex)
+                    {
+                        case 0: //Now
+                        {
+                            objectiveScheduleDate.setValue(objectiveCreateDate);
+                            break;
+                        }
+                        case 1: //Tomorrow
+                        {
+                            //Day starts at 6 AM
+                            objectiveScheduleDate.setValue(objectiveCreateDate.plusDays(1).truncatedTo(ChronoUnit.DAYS).plusHours(6));
+                            break;
+                        }
+                        case 2: //In a week
+                        {
+                            //Day starts at 6 AM
+                            objectiveScheduleDate.setValue(objectiveCreateDate.plusDays(7).truncatedTo(ChronoUnit.DAYS).plusHours(6));
+                            break;
+                        }
+                        case 3: //Custom
+                        {
+                            DatePickerDialog datePickerDialog = new DatePickerDialog(activity);
+                            datePickerDialog.setOnDateSetListener((datePicker, year, month, day) ->
+                            {
+                                //Day starts at 6 AM
+                                //Also Java numerates months from 0, not from 1
+                                LocalDateTime dateTime = LocalDateTime.of(year, month + 1, day, 6, 0, 0);
+                                objectiveScheduleDate.setValue(dateTime);
+                            });
+
+                            datePickerDialog.show();
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+
+                    boolean objectiveRepeatable = false;
+
+                    int objectiveRepeatIndex = repeatSpinner.getSelectedItemPosition();
+                    switch(objectiveRepeatIndex)
+                    {
+                        case 0: //1
+                        {
+                            objectiveRepeatable = false;
+                            break;
+                        }
+                        case 1: //Infinity
+                        {
+                            objectiveRepeatable = true;
+                            break;
+                        }
+                    }
+
+                    int objectiveIntervalIndex = intervalSpinner.getSelectedItemPosition();
+                    switch(objectiveIntervalIndex)
+                    {
+                        case 0: //Day
+                        {
+                            objectiveRepeatDuration = Duration.ofDays(1);
+                            break;
+                        }
+                        case 1: //Week
+                        {
+                            objectiveRepeatDuration = Duration.ofDays(7);
+                            break;
+                        }
+                        case 2: //Month
+                        {
+                            objectiveRepeatDuration = Duration.ofDays(30);
+                            break;
+                        }
+                    }
+
+                    if(objectiveRepeatable)
+                    {
+                        if(occasionalCheckbox.isChecked())
+                        {
+                            objectiveRepeatProbability = 0.5f;
+                        }
+                        else
+                        {
+                            objectiveRepeatProbability = 1.0f;
+                        }
+                    }
+                    else
+                    {
+                        //No repetition!
+                        objectiveRepeatDuration = Duration.ZERO;
+                        objectiveRepeatProbability = 0.0f;
+                    }
+
+                    String configFolder = Objects.requireNonNull(activity.getExternalFilesDir("/app")).getAbsolutePath();
+
+                    TransactionDispatcher transactionDispatcher = new TransactionDispatcher();
+                    transactionDispatcher.setSchedulerCache(mSchedulerCache);
+                    transactionDispatcher.setListCache(mListCache);
+
+                    transactionDispatcher.addObjectiveTransaction(configFolder, objectiveCreateDate, objectiveScheduleDate.getValue(),
+                                                                  objectiveRepeatDuration, objectiveRepeatProbability,
+                                                                  nameText, descriptionText, new ArrayList<>());
+                }
+            }
+        });
+
+        builder.setNegativeButton(R.string.createCancel, new DialogInterface.OnClickListener()
+        {
+            public void onClick(DialogInterface dialog, int id)
+            {
+                //Nothing
+            }
+        });
+
+        resultDialog.setValue(builder.create());
+        return resultDialog.getValue();
+    }
+}
