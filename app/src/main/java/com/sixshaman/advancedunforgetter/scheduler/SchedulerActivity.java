c1b4fc62a733c1a82753a7293561b88837f69e4f
Sixshaman <shaman1234@gmail.com>
2021-03-05 23:33:48 +0300

    Speed dial floating action button refinement

--- app/src/main/java/com/sixshaman/advancedunforgetter/scheduler/SchedulerActivity.java
+++ app/src/main/java/com/sixshaman/advancedunforgetter/scheduler/SchedulerActivity.java
@@ -6,28 +6,19 @@ import android.animation.ObjectAnimator;
 import android.graphics.drawable.Animatable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.*;
-import androidx.appcompat.app.AlertDialog;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import com.sixshaman.advancedunforgetter.R;
-import com.sixshaman.advancedunforgetter.list.ObjectiveListCache;
 import com.sixshaman.advancedunforgetter.utils.LockedReadFile;
-import com.sixshaman.advancedunforgetter.utils.NewObjectiveDialogFragment;
 import com.sixshaman.advancedunforgetter.utils.TransactionDispatcher;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -39,17 +30,13 @@ public class SchedulerActivity extends AppCompatActivity
     //Scheduler cache model
     private ObjectiveSchedulerCache mSchedulerCache;
 
-    //FAB speed dial data (non-refined)
-    //https://stackoverflow.com/questions/35375153/android-floatingactionbutton-speed-dial
-    private static final String TRANSLATION_Y = "translationY";
-    private ImageButton fab;
-    private boolean expanded = false;
-    private View fabAction1;
-    private View fabAction2;
-    private View fabAction3;
-    private float offset1;
-    private float offset2;
-    private float offset3;
+    //https://stackoverflow.com/a/44508892
+    private boolean mFabExpanded;
+
+    //Dynamic offset to speed dial fab elements
+    private float mFabPoolOffset;
+    private float mFabChainOffset;
+    private float mFabObjectiveOffset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -61,34 +48,31 @@ public class SchedulerActivity extends AppCompatActivity
 
         setTitle(R.string.title_activity_task_scheduler);
 
-        final ViewGroup fabContainer = (ViewGroup) findViewById(R.id.fab_container);
+        mFabExpanded = false;
 
-        fabAction1 = findViewById(R.id.fab_action_1);
-        fabAction1.setOnClickListener(this::fabAction1);
+        final ViewGroup fabContainer = findViewById(R.id.fab_container);
 
-        fabAction2 = findViewById(R.id.fab_action_2);
-        fabAction2.setOnClickListener(this::fabAction2);
+        final View fabAddPool      = findViewById(R.id.fab_add_pool);
+        final View fabAddChain     = findViewById(R.id.fab_add_chain);
+        final View fabAddObjective = findViewById(R.id.fab_add_objective);
 
-        fabAction3 = findViewById(R.id.fab_action_3);
-        fabAction2.setOnClickListener(this::fabAction2);
-
-        fab = (ImageButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
+        final ImageButton fabSpeedDial = findViewById(R.id.mainFabButton);
+        fabSpeedDial.setOnClickListener(view ->
         {
-            @Override
-            public void onClick(View v)
+            mFabExpanded = !mFabExpanded;
+            if(mFabExpanded)
             {
-                expanded = !expanded;
-                if (expanded)
-                {
-                    expandFab();
-                }
-                else
-                {
-                    collapseFab();
-                }
+                expandFab(fabSpeedDial, fabAddPool, fabAddChain, fabAddObjective);
+            }
+            else
+            {
+                collapseFab(fabSpeedDial, fabAddPool, fabAddChain, fabAddObjective);
             }
         });
+
+        fabAddPool.setOnClickListener(this::addObjectivePool);
+        fabAddChain.setOnClickListener(this::addObjectiveChain);
+        fabAddObjective.setOnClickListener(this::addObjective);
 
         fabContainer.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener()
         {
@@ -96,76 +80,78 @@ public class SchedulerActivity extends AppCompatActivity
             public boolean onPreDraw()
             {
                 fabContainer.getViewTreeObserver().removeOnPreDrawListener(this);
-                offset1 = fab.getY() - fabAction1.getY();
-                fabAction1.setTranslationY(offset1);
-                offset2 = fab.getY() - fabAction2.getY();
-                fabAction2.setTranslationY(offset2);
-                offset3 = fab.getY() - fabAction3.getY();
-                fabAction3.setTranslationY(offset3);
+
+                mFabPoolOffset      = fabSpeedDial.getY() - fabAddPool.getY();
+                mFabChainOffset     = fabSpeedDial.getY() - fabAddChain.getY();
+                mFabObjectiveOffset = fabSpeedDial.getY() - fabAddObjective.getY();
+
+                fabAddPool.setTranslationY(mFabPoolOffset);
+                fabAddChain.setTranslationY(mFabChainOffset);
+                fabAddObjective.setTranslationY(mFabObjectiveOffset);
+
                 return true;
             }
         });
     }
 
-    private void collapseFab()
+    private void collapseFab(final ImageButton fab, final View fabAddPool, final View fabAddChain, final View fabAddObjective)
     {
-        fab.setImageResource(R.drawable.animated_minus);
+        fab.setImageResource(R.drawable.animated_cross);
+
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(createCollapseAnimator(fabAction1, offset1),
-                                 createCollapseAnimator(fabAction2, offset2),
-                                 createCollapseAnimator(fabAction3, offset3));
+        animatorSet.playTogether(createCollapseAnimator(fabAddPool,      mFabPoolOffset),
+                                 createCollapseAnimator(fabAddChain,     mFabChainOffset),
+                                 createCollapseAnimator(fabAddObjective, mFabObjectiveOffset));
         animatorSet.start();
-        animateFab();
+
+        animateSpeedDialFab(fab);
     }
 
-    private void expandFab()
+    private void expandFab(final ImageButton fab, final View fabAddPool, final View fabAddChain, final View fabAddObjective)
     {
         fab.setImageResource(R.drawable.animated_plus);
+
         AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(createExpandAnimator(fabAction1, offset1),
-                                 createExpandAnimator(fabAction2, offset2),
-                                 createExpandAnimator(fabAction3, offset3));
+        animatorSet.playTogether(createExpandAnimator(fabAddPool,      mFabPoolOffset),
+                                 createExpandAnimator(fabAddChain,     mFabChainOffset),
+                                 createExpandAnimator(fabAddObjective, mFabObjectiveOffset));
         animatorSet.start();
-        animateFab();
+
+        animateSpeedDialFab(fab);
     }
 
     private Animator createCollapseAnimator(View view, float offset)
     {
-        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, 0, offset)
-                .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+        return ObjectAnimator.ofFloat(view, "translationY", 0, offset).setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
     }
 
     private Animator createExpandAnimator(View view, float offset)
     {
-        return ObjectAnimator.ofFloat(view, TRANSLATION_Y, offset, 0)
-                .setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
+        return ObjectAnimator.ofFloat(view, "translationY", offset, 0).setDuration(getResources().getInteger(android.R.integer.config_mediumAnimTime));
     }
 
-    private void animateFab()
+    private void animateSpeedDialFab(ImageButton fab)
     {
         Drawable drawable = fab.getDrawable();
         if(drawable instanceof Animatable)
         {
-            ((Animatable) drawable).start();
+            ((Animatable)drawable).start();
         }
     }
 
-    public void fabAction1(View view)
+    public void addObjectivePool(View view)
     {
-        Log.d(TAG, "Action 1");
-        Toast.makeText(this, "Go shopping!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Add objective pool", Toast.LENGTH_SHORT).show();
     }
 
-    public void fabAction2(View view)
+    public void addObjectiveChain(View view)
     {
-        Log.d(TAG, "Action 2");
-        Toast.makeText(this, "Gimme money!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Add objective chain", Toast.LENGTH_SHORT).show();
     }
 
-    public void fabAction3(View view)
+    public void addObjective(View view)
     {
-        Log.d(TAG, "Action 3");
-        Toast.makeText(this, "Turn it up!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Add objective", Toast.LENGTH_SHORT).show();
     }
 
     @Override
